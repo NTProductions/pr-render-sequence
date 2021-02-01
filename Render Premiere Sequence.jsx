66d6c0e26368def88205d6724dc05f16d65e77db
Nate Lovell <Natelovell99@gmail.com>
2021-02-01 04:38:30 -0700

    Create Render Premiere Sequence.jsx

---
+++ Render Premiere Sequence.jsx
@@ -0,0 +1,11 @@
+﻿// premiere render sequence
+var project = app.project;
+var sequence = project.activeSequence;
+
+app.encoder.launchEncoder();
+$.writeln(sequence.name);
+$.writeln(File("C:\\Users\\12082\\Documents\\Adobe\\Adobe Media Encoder\\14.0\\Presets\\IG.epr").exists);
+
+app.encoder.encodeSequence(sequence, "C:\\Users\\12082\\Desktop\\test.mp4", "C:\\Users\\12082\\Documents\\Adobe\\Adobe Media Encoder\\14.0\\Presets\\Max Q.epr", 0, true);
+
+app.encoder.startBatch();
